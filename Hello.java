80bceb246f2718a1495f984a6532bba5e4d7856d
Lee-HS357 <ihs03@naver.com>
2024-08-06 12:30:47 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,9 +5,9 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/** 
-		 * 작성일 :
-		 * 작성자 :
-		 * 내용 :
+		 * 작성일 : 2024 - 08 - 06
+		 * 작성자 : 이현수	
+		 * 내용 : Hello Java를 10번 출력하는 프로그램 작성
 		 */
 		System.out.println("Hello Java");
 		System.out.println("Hello Java");
